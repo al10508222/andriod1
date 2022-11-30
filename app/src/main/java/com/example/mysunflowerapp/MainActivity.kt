fe93ae84149aa258a4d8cb9f383dda4440a0e93b
gamoma@live.com.mx <gamoma@live.com.mx>
2022-11-29 23:28:45 -0600

    primera carga andriod

---
+++ app/src/main/java/com/example/mysunflowerapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.mysunflowerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.graphics.Color
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var boton1:Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        boton1 = findViewById(R.id.button1)
+
+        boton1.setOnClickListener {
+            if(boton1.text != "Soy Azul"){
+                boton1.setBackgroundColor(Color.parseColor("#0352F0"))
+                boton1.setText("Soy Axul")
+            }
+            else if(boton1.text != "Soy Rojo"){
+                boton1.setBackgroundColor(Color.parseColor("#F01C03"))
+                boton1.setText("Soy Rojo")
+            }
+            else{
+                boton1.setBackgroundColor(Color.parseColor("#E2F003"))
+                boton1.setText("Soy Amarillo")
+            }
+        }
+
+
+    }
+}
