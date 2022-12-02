ed1cdeb3c38f9743bb58be3686893c2d17a3d085
gamoma@live.com.mx <gamoma@live.com.mx>
2022-12-01 18:15:51 -0600

    pasar datos entre activities

--- app/src/main/java/com/example/mysunflowerapp/ActividadDos.kt
+++ app/src/main/java/com/example/mysunflowerapp/ActividadDos.kt
@@ -2,10 +2,16 @@ package com.example.mysunflowerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class ActividadDos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_actividad_dos)
+
+        val mensaje_recibido:String = intent.getStringExtra("TEXTO").toString()
+        var mensaje: TextView = findViewById(R.id.mensaje)
+        mensaje.text = mensaje_recibido
     }
 }
